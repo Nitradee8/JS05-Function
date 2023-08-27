b9da49406afc85b291a9d60868371bf6f273f8d0
Nitradee8 <nbtt.nik@gmail.com>
2023-08-28 00:04:13 +0700

    update

---
+++ Special-Exercise/01/main.js
@@ -0,0 +1,23 @@
+function draw(n) {
+    for (let i = 1; i <= n; i++) {
+      console.log('*'.repeat(n));
+    }
+  }
+  
+  draw(2);
+
+  function draw(n) {
+    for (let i = 1; i <= n; i++) {
+      console.log('*'.repeat(n));
+    }
+  }
+  
+  draw(3);
+
+  function draw(n) {
+    for (let i = 1; i <= n; i++) {
+      console.log('*'.repeat(n));
+    }
+  }
+  
+  draw(4);
