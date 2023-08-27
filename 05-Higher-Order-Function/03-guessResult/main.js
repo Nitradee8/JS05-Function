87f3024fac66b9c72c0a6c825880f68189be9a3e
Nitradee8 <nbtt.nik@gmail.com>
2023-08-27 22:16:35 +0700

    update

--- 05-Higher-Order-Function/03-guessResult/main.js
+++ 05-Higher-Order-Function/03-guessResult/main.js
@@ -1,4 +1,4 @@
-ผลลัพธ์ในบรรทัดที่มี * มีค่าเป็นอะไรและเพราะอะไร
+// ผลลัพธ์ในบรรทัดที่มี * มีค่าเป็นอะไรและเพราะอะไร
 
 function makeWorker() {
   let name = 'Pete';
@@ -10,40 +10,3 @@ let name = 'John';
 let work = makeWorker();
 work(); // * "Pete" เพราะ ฟังก์ชัน makeWorker ถูกเรียกและแสดงค่าตัวแปร name ออกมา 
 
-
-
-//-------------------------------------------------------------------------------------
-
-// function multiply(num1, num2, callback) {
-//     let result = num1 * num2;
-//     callback(result);
-// }
-
-// function displayResult(result) {
-//     console.log('The result is: ' + result);
-// }
-
-// multiply(5, 10, displayResult);
-
-// function outerFunction() {
-//     let outerVariable = 'Hello';
-
-//     function innerFunction() {
-//         console.log(outerVariable + ' World!');
-//     }
-
-//     return innerFunction;
-// }
-
-// let greeting = outerFunction();
-// greeting(); // Output: "Hello World!"
-
-// function adder(a) {
-//     return function (b) {
-//         return a + b;
-//     };
-// }
-
-// let add5 = adder(5);
-// console.log(add5(3)); // Output: 8
-// console.log(add5(7)); // Output: 12
