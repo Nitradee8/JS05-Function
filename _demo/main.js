b35584108a87d78feb0caf881ee36c17f689f41f
Nitradee8 <nbtt.nik@gmail.com>
2023-08-27 21:39:51 +0700

    update

--- _demo/main.js
+++ _demo/main.js
@@ -1,5 +1,98 @@
-function sayHelloWorld (){
-    alert("Hello World")
-}
+// function sayHelloWorld (){
+//     alert("Hello World")
+// }
 
-sayHelloWorld()
+// sayHelloWorld()
+
+// test
+// test 2
+
+// console.log('เสียงงง นกกรงหัวจุกมันริก');
+// console.log('จก ');
+// console.log('กวิ๊ก ๆๆๆๆๆๆๆ');
+
+//-------------------------------------------------------------------------------
+// function soundOfBird() {
+//     console.log('เสียงงง นกกรงหัวจุกมันริก');
+//     console.log('จก ');
+//     console.log('กวิ๊ก ๆๆๆๆๆๆๆ');
+// }
+// soundOfBird();
+// soundOfBird();
+// soundOfBird();
+
+//-------------------------------------------------------------------------------
+// function soundOfBird() {
+//     console.log('เสียงงง นกกรงหัวจุกมันริก');
+//     console.log('จก ');
+//     for (let i = 1; i < = 11; i++);
+//     console.log('กวิ๊ก ๆๆๆๆๆๆๆ');
+// }
+// soundOfBird();
+
+//-------------------------------------------------------------------------------
+function isPrime() {
+    let n = 5;
+    let isPrime = true;
+    for (let divider = 2; divider < n; divider++) {
+        if (n % divider == 0) {
+        isPrime = false;
+        brack;
+         }
+    }
+    if (isPrime) console.log(n);
+}
+isPrime();
+isPrime();
+isPrime();
+
+// //-----------------------------------------------------------------------
+// function announcement(stationName, doorSide) {
+//     console.log(`next station ${stationName}.`);
+//     console.log(`the door will open on the ${doorSide} hand side`);
+//    }
+   
+//    announcement("Asoke", "right" ) 
+//    // next station Asoke. 
+//    // The door will open on the right hand side.
+
+// //-----------------------------------------------------------------------
+//    function announcement(stationName, doorSide) {
+//     console.log(`next station ${stationName}.`);
+//     console.log(`the door will open on the ${doorSide} hand side`);
+//    }
+   
+//    announcement("right", "Asoke" ) 
+//    // next station right. 
+//    // The door will open on the Asoke hand side.
+   
+//-----------------------------------------------------------------------
+// function addTwo(n) {
+//     let result = n + 2;
+//      console.log('Before Return')
+//     return result;
+//     console.log('End calc');
+//     }
+
+// let result = addTwo(5);
+// console.log(result);
+
+// number => bool
+// function isPrime(num) {
+//     for (let d = 2; d < num; d++) {
+//         // console.log(d);
+//         if (num % d == 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// function printPrime(limit) {
+//     for (let n = 2; n <= limit; n++) {
+//         if (isPrime(n)) {
+//             console.log(n);
+//         }
+//     }
+// }
+// printPrime(50);
+
