89bbee9c3e99ffe20bef02f35d94354905d554ea
Nitradee8 <nbtt.nik@gmail.com>
2023-08-28 00:16:29 +0700

    update

---
+++ Special-Exercise/02/main.js
@@ -0,0 +1,42 @@
+// จงเขียน method draw(int n) ให้ print ออกมา ในกรณีที่ n มีค่าเท่ากับ 2 ผลลัพธ์ดังนี้
+// **
+// **
+
+// function draw(n) {
+//     for (let i = 1; i <= n; i++) {
+//       console.log('*'.repeat(n));
+//     } 
+//   }
+  
+//   draw(2);
+
+
+// จงเขียน method draw(int n) ให้ print ออกมา ในกรณีที่ n มีค่าเท่ากับ 2 ผลลัพธ์ดังนี้
+// ***
+// ***
+// ***
+
+//   function draw(n) {
+//     for (let i = 1; i <= n; i++) {
+//       console.log('*'.repeat(n));
+//     } 
+
+//   }
+  
+//   draw(3);
+
+
+// จงเขียน method draw(int n) ให้ print ออกมา ในกรณีที่ n มีค่าเท่ากับ 2 ผลลัพธ์ดังนี้
+// ****
+// ****
+// ****
+// ****
+
+  function draw(n) {
+    for (let i = 1; i <= n; i++) {
+      console.log('*'.repeat(n));
+    } 
+
+  }
+  
+  draw(4);
